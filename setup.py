8bbeffe23568ea65fc44ba9b5323425ff709d8c9
Jared Hendrickson <jaredhendrickson13@gmail.com>
2022-11-13 23:00:26 -0700

    build!: deprecate python3.6 support, it is eol

--- setup.py
+++ setup.py
@@ -47,5 +47,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
