b8db799f1b61f52f2ed622b6be4119d412ad09a1
Jared Hendrickson <jaredhendrickson13@gmail.com>
2024-05-23 10:31:42 -0600

    tests: correct pylint errors in test files

--- simple_acme_dns/tests/test_simple_acme_dns.py
+++ simple_acme_dns/tests/test_simple_acme_dns.py
@@ -196,14 +196,18 @@ class TestSimpleAcmeDns(unittest.TestCase):
 
     def test_email_property_when_email_is_set(self):
         """Checks that the email property is set correctly."""
-        self.client._email = "test@example.com"
+        self.client.email = "test@example.com"
         self.assertEqual(self.client.email, "test@example.com")
 
     def test_email_property_when_email_is_not_set(self):
         """Checks that the email property raises an error when not set."""
         with self.assertRaises(simple_acme_dns.errors.InvalidEmail):
-            self.client._email = None
-            return self.client.email
+            client = simple_acme_dns.ACMEClient(
+                domains=TEST_DOMAINS,
+                directory=TEST_DIRECTORY,
+                nameservers=TEST_NAMESERVERS
+            )
+            return client.email
 
 
 if __name__ == '__main__':
